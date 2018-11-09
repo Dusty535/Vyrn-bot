1ebdd544c9cc8bb54b9f7abfd6efb8fbba19fcea
Dusty535 <44588663+Dusty535@users.noreply.github.com>
2018-11-09 17:53:34 +0100

    Update emo.js

--- commands/emo.js
+++ commands/emo.js
@@ -2,10 +2,7 @@ exports.run = (client, message, args) => {
 if (args.length ==0){
     message.channel.send("https://gbf.wiki/Stickers#English");
 }
-if (args == 'list'){
-message.channel.send('Mdr t'a cru il y a BEAUCOUP d'émotes go sur https://gbf.wiki/Stickers#English')
-  
-}
+    
 const fs = require('fs');
 let path = "./emo/"+args+".png"
 let remp = /,/gi
